7ffa3ae24aec5165e4174dc74478368f93d738b5
anonyme hacker <anonyme201504@gmail.com>
2024-05-30 14:36:24 -0300

    tv

--- lib/assets/whattodocheriefm.rb
+++ lib/assets/whattodocheriefm.rb
@@ -63,41 +63,55 @@ j.to_h
 end
 @all=@y.sort{|x|Time.parse(x["time"]).to_i}
 req = "require 'time'\nrequire 'open-uri'\nrequire 'active_record'\nrequire 'timeout'\nrequire 'nokogiri'\n"
-ok=("""cat <<EOF > cut.rb 
-\n#{req}\n
+ok=("""cat <<EOF > cut.rb\n#{req}\n
 def hack(nombresecondes)
 \n@time_end=Time.parse('#{@time_end}')\n@all=#{@all.to_s}\n
 temps = @all.reverse #.sort {|x,y| y['time'] <=> x['time']} #.pluck('time')
 \n0.upto(temps.length - 2).each.with_index do |nmusiques,i|
-\n  if nmusiques == 0
-\n    t2='00:00'
-\n  else
-\n    tinsec = (@time_end - Time.parse(temps[nmusiques-1]['time'])) + 60.to_f + (nombresecondes.to_f)
-\n    t=tinsec.to_s
-\n    t2 = DateTime.strptime(t, '%s').strftime('%M:%S')
-\n  end
-\n  tinsec = (@time_end - Time.parse(temps[nmusiques]['time'])) + 60.to_f + (nombresecondes.to_f)
+\n  #if nmusiques == 0
+\n  #  t2='00:00'
+\n  #else
+\n  tinsec = (@time_end - Time.parse(temps[nmusiques-1]['time']))# + 60.to_f + (nombresecondes.to_f)
+\n  t=tinsec.to_s
+\n  t2 = DateTime.strptime(t, '%s').strftime('%M:%S')
+\n  #end
+\n  tinsec = (@time_end - Time.parse(temps[nmusiques]['time']))# + 60.to_f + (nombresecondes.to_f)
 \n  t=tinsec.to_s
 \n  t1 = DateTime.strptime(t, '%s').strftime('%M:%S')
 \n  #p @time_end
 \n  #p temps[nmusiques]
 \n  nom = temps[nmusiques]['video'].parameterize
 \n  if nom.strip.length > 0
-\n    cut=\"\"\"float=\\\$(ffprobe -i tv.mp4 -show_entries format=duration -v quiet -of csv=\\\"p=0\\\")
-\n    \idur=\\\${float%.*}
-\n    \ntrim=\\\$((dur - \#\{t2.split(\":\")[0].to_i*60+t2.split(\":\")[1].to_i\}))
-\n    \ntrim2=\\\$((dur - \#\{i == 0 ? 0 : (t1.split(\":\")[0].to_i*60+t1.split(\":\")[1].to_i)\}))
-\n    \nffmpeg -ss \\\$trim -t \\\$trim2 -i tv.mp4 \#{nom}.mp4
-\n    \nmid3v2 -a \#\{temps[nmusiques]['title'].dump\} -t \#\{temps[nmusiques]['title'].dump\} \#\{nom\}.mp4
-\n    \"\"\"
-\n    system(cut)
+\n    cut=\"\"\"#!/bin/bash -x
+\nfloat=\\\$(ffprobe -i tv.mp4 -show_entries format=duration -v quiet -of csv=\\\"p=0\\\")
+\ndur=\\\${float%.*}
+\ntrim=\\\$((dur - \#\{t2.split(\":\")[0].to_i*60+t2.split(\":\")[1].to_i\}))
+\ntrim2=\\\$((dur - \#\{i == 0 ? 0 : (t1.split(\":\")[0].to_i*60+t1.split(\":\")[1].to_i)\}))
+\nif [ \\\$trim -lt 0 ]; then
+    \ntrim=0
+\nfi
+\nif [ \\\"\\\$trim2\\\" -gt \\\"\\\$trim\\\" ]; then
+    \nif [ \\\"\\\$trim2\\\" -ge 0 ]; then
+              \nif [ \\\"\\\$trim\\\" -ge 0 ]; then
+                  \necho \\\"You are hacker\\\"
+                  \necho \\\"\\\$trim2 > \\\$trim \\\"
+                  \necho \\\"ffmpeg -ss \\\$trim -t \\\$trim2 -i tv.mp4 \#{nom}.mp4\\\"
+                  \nffmpeg -ss \\\$trim -t \\\$trim2 -i tv.mp4 \#{nom}.mp4
+              \nfi
+    \nfi
+\nfi
+\"\"\"
+\n    File.write(\"cut.sh\",cut)
+\n    system(\"chmod +x cut.sh\")
+\n    system(\"./cut.sh\")
 \n  end
 \nend
 \nend
-\nhack(-460)
+\nhack(0)
 \n#mydate and time : lib/assets/history/#{@date}/
 \nEOF\n\necho heeeey\nls\nmkdir -p #{@date}; mv cut.rb #{@date}; mv tv.mp4 #{@date};(cd #{@date} && ruby cut.rb);\n
-""");
+"""); 
+p (ok)
 system(ok)
   end
 end
