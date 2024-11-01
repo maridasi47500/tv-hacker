b4a1cd0ff9be28a2d5f7a7cbb98171ab1d8b1daf
maridasi47500 <61146153+maridasi47500@users.noreply.github.com>
2024-11-01 11:23:26 -0300

    Update mytv.sh

--- lib/assets/mytv.sh
+++ lib/assets/mytv.sh
@@ -1,1 +1,2 @@
-mpv "$1" --geometry=963x542+1+37 --vo=null --ao=null --no-resume-playback --stream-record=tv.mp4
+#mpv "$1" --geometry=963x542+1+37 --vo=null --ao=null --no-resume-playback --stream-record=tv.mp4
+vlc "$1" --sout='#standard{access=file,mux=ts,dst=tv.mp4}' --no-sout-all --sout-keep
